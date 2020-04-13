1d717dd274c67b39eb66fac604745daa6e5dc853
Kristoffer Noga <k.noga@hotmail.com>
2020-04-13 13:01:18 +0200

    added styles

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,27 +1,36 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Typography, Button } from '@material-ui/core'
+import { Typography, Button, Paper, makeStyles } from '@material-ui/core'
 import { getPosts } from '../api/dataApi'
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        padding: theme.spacing(3)
+    }
+}));
+
 function Posts() {
+    const classes = useStyles()
     const dispatch = useDispatch()
     const posts = useSelector(state => state.data.posts)
     useEffect(() => {
         console.log("gettings posts");
         dispatch(getPosts())
     }, [dispatch])
-    
+
     return (
         <React.Fragment>
-            <Typography component="h2" variant="h4">Posts <small>{posts.length}</small></Typography>
-            <Button variant="outlined" onClick={() => dispatch(getPosts())}>
-                More posts
+            <Paper className={classes.paper}>
+                <Typography component="h2" variant="h4">Posts <small>{posts.length}</small></Typography>
+                <Button variant="outlined" onClick={() => dispatch(getPosts())}>
+                    More posts
             </Button>
-            {
-                posts.map((p, i) => {
-                    return <p key={i}>{p.title}</p>
-                })
-            }
+                {
+                    posts.map((p, i) => {
+                        return <p key={i}>{p.title}</p>
+                    })
+                }
+            </Paper>
         </React.Fragment>
     )
 }
